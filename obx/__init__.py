4041f78c5e1a3458d83a2a625e35af1f4fe7c8e5
Bart Thate <rssbotd@gmail.com>
2024-12-02 22:13:30 +0100

    add obx

--- obx/__init__.py
+++ obx/__init__.py
@@ -114,62 +114,6 @@ def match(obj, txt):
             yield key
 
 
-def parse(obj, txt=None) -> None:
-    if txt is None:
-        txt = ""
-    args = []
-    obj.args    = []
-    obj.cmd     = ""
-    obj.gets    = Obj()
-    obj.hasmods = False
-    obj.index   = None
-    obj.mod     = ""
-    obj.opts    = ""
-    obj.result  = []
-    obj.sets    = Obj()
-    obj.txt     = txt or ""
-    obj.otxt    = obj.txt
-    _nr = -1
-    for spli in obj.otxt.split():
-        if spli.startswith("-"):
-            try:
-                obj.index = int(spli[1:])
-            except ValueError:
-                obj.opts += spli[1:]
-            continue
-        if "==" in spli:
-            key, value = spli.split("==", maxsplit=1)
-            val = getattr(obj.gets, key, None)
-            if val:
-                value = val + "," + value
-                setattr(obj.gets, key, value)
-            continue
-        if "=" in spli:
-            key, value = spli.split("=", maxsplit=1)
-            if key == "mod":
-                obj.hasmods = True
-                if obj.mod:
-                    obj.mod += f",{value}"
-                else:
-                    obj.mod = value
-                continue
-            setattr(obj.sets, key, value)
-            continue
-        _nr += 1
-        if _nr == 0:
-            obj.cmd = spli
-            continue
-        args.append(spli)
-    if args:
-        obj.args = args
-        obj.txt  = obj.cmd or ""
-        obj.rest = " ".join(obj.args)
-        obj.txt  = obj.cmd + " " + obj.rest
-    else:
-        obj.txt = obj.cmd or ""
-    return obj
-
-
 def search(obj, selector, matching=None):
     res = False
     if not selector:
@@ -273,9 +217,7 @@ def dumps(*args, **kw):
 
 def __dir__():
     return (
-        'Config',
         'Object',
-        'Obj',
         'construct',
         'dumps',
         'edit',
@@ -283,7 +225,6 @@ def __dir__():
         'loads',
         'items',
         'match',
-        'parse',
         'search',
         'update',
         'values'
