5823cc45a111c576651921da2839fe50c7841a53
Bart Thate <rssbotd@gmail.com>
2024-12-05 14:32:07 +0100

    update runtime

--- obz/runtime.py
+++ obz/runtime.py
@@ -17,8 +17,17 @@ import _thread
 
 class Default:
 
+    def __contains__(self, key):
+        return key in dir(self)
+
     def __getattr__(self, key):
         return self.__dict__.get(key, "")
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
 
 
 class Errors:
