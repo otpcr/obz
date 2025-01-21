beb8c58a53286195d0582a25a6d9fa770dfdca25
Bart Thate <bthate@dds.nl>
2025-01-21 19:43:21 +0100

    fix ruff

--- obz/modules/face.py
+++ obz/modules/face.py
@@ -1,8 +1,12 @@
 # This file is placed in the Public Domain.
+# ruff: noqa: F401
 
 
 "interface"
 
+
+from . import cmd, err, flt, fnd, irc, log, mbx, mdl, mod, opm, req, rss, rst
+from . import slg, tdo, thr, tmr, udp, upt, wsd
 
 
 
